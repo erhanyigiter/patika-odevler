ccf7e75bf04119e0168b32389b48b92553fccd1e
erhanyigiter <erhanyigit.er@outlook.com>
2023-10-23 23:52:00 +0300

    hesap-makinesi
    Kullanıcıdan aldığı iki sayıyı kullanıcının isteğine göre çarpma, bölme, toplama veya çıkartma işlemi sağlayan program.

---
+++ hesap-makinesi/src/hesapmakinesi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class hesapmakinesi {
+    public static void main(String[] args) {
+        int n1,n2;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz: ");
+        n1 = input.nextInt();
+        System.out.print("İkinci sayıyı giriniz: ");
+        n2 = input.nextInt();
+
+        
+
+
+
+
+
+    }
+}
